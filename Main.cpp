007132f5b575d7add00605b2bd7031f12c857d5c
Michael Hughes <michael.hughes94@btinternet.com>
2018-08-02 22:28:12 +0100

    Add Opponent
    Now you can play against a simple, but also unbeatable, computer
    controlled player.

--- Main.cpp
+++ Main.cpp
@@ -2,9 +2,10 @@
 
 int main()
 {
-  sf::RenderWindow window{sf::VideoMode{500, 500}, "SFML works!"};
+  sf::RenderWindow window{sf::VideoMode{500, 500}, "Pong"};
 
   sf::CircleShape ball{5.f};
+  ball.setPosition(100, 50);
   ball.setFillColor(sf::Color::Green);
   sf::Vector2f ballVelocity{0.1f, 0.1f};
 
@@ -14,6 +15,16 @@ int main()
   sf::Vector2f playerVelocity{0.f, 0.f};
   bool movePlayerUp{false};
   bool movePlayerDown{false};
+
+  sf::RectangleShape opponent{sf::Vector2f{10.f, 100.f}};
+  opponent.setPosition(window.getSize().x - (10 + opponent.getGlobalBounds().width), 50);
+  opponent.setFillColor(sf::Color::Blue);
+  sf::Vector2f opponentVelocity{0.f, 0.f};
+
+  // sf::Font font;
+  // font.loadFromFile("C:\Windows\Fonts\Arial.ttf");
+  // sf::Text text;
+  // text.setString("")
 
   while (window.isOpen())
   {
@@ -48,6 +59,21 @@ int main()
       }
     }
 
+    // Set Opponent velocity
+    if (ball.getPosition().y < opponent.getGlobalBounds().top + opponent.getGlobalBounds().height / 2)
+    {
+      opponentVelocity.y = -0.1f;
+    }
+    else if (ball.getPosition().y > opponent.getGlobalBounds().top + opponent.getGlobalBounds().height / 2)
+    {
+      opponentVelocity.y = 0.1f;
+    }
+    else
+    {
+      opponentVelocity.y = 0.f;
+    }
+
+    // Set Player velocity
     if (movePlayerUp)
     {
       playerVelocity.y = -0.1f;
@@ -63,10 +89,54 @@ int main()
 
     player.move(playerVelocity);
     ball.move(ballVelocity);
+    opponent.move(opponentVelocity);
+
+    // Handle Ball-Wall collision
+    if (ball.getGlobalBounds().left < 0 && ballVelocity.x < 0 || ball.getGlobalBounds().left + ball.getGlobalBounds().width > window.getSize().x && ballVelocity.x > 0)
+    {
+      ballVelocity.x *= -1;
+    }
+    if (ball.getGlobalBounds().top < 0 && ballVelocity.y < 0 || ball.getGlobalBounds().top + ball.getGlobalBounds().height > window.getSize().y && ballVelocity.y > 0)
+    {
+      ballVelocity.y *= -1;
+    }
+
+    // Handle Player-Wall collision
+    if (player.getGlobalBounds().top < 0)
+    {
+      player.setPosition(player.getPosition().x, 0);
+    }
+    else if (player.getGlobalBounds().top + player.getGlobalBounds().height > window.getSize().y)
+    {
+      player.setPosition(player.getPosition().x, window.getSize().y - player.getGlobalBounds().height);
+    }
+
+    // Handle Opponent-Wall collision
+    if (opponent.getGlobalBounds().top < 0)
+    {
+      opponent.setPosition(opponent.getPosition().x, 0);
+    }
+    else if (opponent.getGlobalBounds().top + opponent.getGlobalBounds().height > window.getSize().y)
+    {
+      opponent.setPosition(opponent.getPosition().x, window.getSize().y - opponent.getGlobalBounds().height);
+    }
+
+    // Handle Player-Ball collision
+    if (player.getGlobalBounds().intersects(ball.getGlobalBounds()) && ballVelocity.x < 0)
+    {
+      ballVelocity.x *= -1;
+    }
+
+    // Handle Opponent-Ball collision
+    if (opponent.getGlobalBounds().intersects(ball.getGlobalBounds()) && ballVelocity.x > 0)
+    {
+      ballVelocity.x *= -1;
+    }
 
     window.clear();
     window.draw(ball);
     window.draw(player);
+    window.draw(opponent);
     window.display();
   }
 }
